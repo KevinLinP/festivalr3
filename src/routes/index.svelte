07ad8afb8e25bfac5df2c315213d7c4df70695da
Kevin Lin <kevin.lin.p@gmail.com>
2022-02-27 19:25:37 -0500

    tweak header spacing

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,9 +4,9 @@
 </script>
 
 <div class="container">
-  <header class="pt-2 pb-4 d-flex align-items-baseline">
-    <h1 class="display-4 font-weight-thin">Movement 2022</h1>
-    <h2 class="display-5 font-weight-thin text-muted ms-3">Teaser Lineup</h2>
+  <header class="pt-2 pb-4 d-flex align-items-baseline flex-wrap">
+    <h1 class="display-4 font-weight-thin me-3">Movement 2022</h1>
+    <h2 class="display-5 font-weight-thin text-muted">Teaser Lineup</h2>
   </header>
 
   {#each artists as artist (artist.name)}
